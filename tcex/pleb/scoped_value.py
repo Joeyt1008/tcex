62ff9ccc2e89870ea51fcb536cced2252f16d0ad
Chris Blades <cblades@threatconnect.com>
2021-11-03 10:16:24 -0400

    commenting service registry and scoped value

--- tcex/pleb/scoped_value.py
+++ tcex/pleb/scoped_value.py
@@ -1,3 +1,4 @@
+"""Declares a scoped_value decorator."""
 # standard library
 import os
 import threading
@@ -10,22 +11,42 @@ T = typing.TypeVar('T')
 
 
 class scoped_value(typing.Generic[T]):
+    """Makes a value unique for each thread.
+
+    Essentially, a thread-and-process local value.  When used to decorate a function, will
+    treat that function as a factory for the underlying value, and will invoke it to produce a value
+    for each thread the value is requested from.
+
+    Note that this also provides a cache: each thread will re-use the value previously created
+    for it.
+    """
     def __init__(self):
+        """Initialize."""
         self.value = threading.local()
 
     @wrapt.decorator
     def __call__(self, wrapped, instance, args, kwargs):
+        """Return or create and return a value for the calling thread.
+
+        The wrapped function is treated as a factory, and invoked with the given args and kwargs.
+        The wrapped function can be a bound method and will work as expected.
+        """
         if hasattr(self.value, 'data'):
+            #  A value has been created for this thread already, but we have to make sure we're in
+            # the same process (threads are duplicated when a process is forked).
             pid, value = self.value.data
             if pid != os.getpid():
                 return self._create_value(wrapped, args, kwargs)
 
             return value
         else:
+            # A value has *not* been created for the calling thread yet, so use the factory to
+            # create a new one.
             new_value = self._create_value(wrapped, args, kwargs)
             return new_value
 
     def _create_value(self, wrapped, args, kwargs):
+        """Call the wrapped factory function to get a new value."""
         data = wrapped(*args, **kwargs)
         setattr(self.value, 'data', (os.getpid(), data))
         return data
--- tcex/registry/service_registry.py
+++ tcex/registry/service_registry.py
@@ -1,19 +1,56 @@
+"""A service registry."""
 # standard library
-from typing import Callable, Type, Union, Tuple
+from typing import Callable, Type, Union, Tuple, Any
 from collections.abc import Container
 
 
 class ServiceRegistry(Container):
+    """Dynamic service registry that supports raw values, factories, and factory providers.
+
+    Terms:
+        Service - An object that is registered with this registry and can be retrieved from it by
+            type or name.
+        Factory - A callable that can return an instance that fulfills a type or name.
+        Provider - An object with one or more members that are factories (as denoted by the
+            @factory decorator)
+    """
     def __init__(self):
+        """."""
         self._values = {}
 
-    def add_service(self, type_or_name: Union[str, Type], value):
+    def add_service(self, type_or_name: Union[str, Type], value: Any):
+        """Add an instance of a type to this registry.
+
+        A service is a single instance of the given type.
+
+        Args:
+            type_or_name: the concrete type of the provided service, or a name (MyClass or
+            'MyClass')
+            value: The service.
+        """
         self._add(type_or_name, value)
 
     def add_factory(self, type_or_name: Union[str, Type], factory: Callable, singleton=False):
+        """Add a factory for a service.
+
+        A factory is any callable that can be invoked to provide the given type of service.
+
+        Args:
+            type_or_name: the concrete type of the provided service, or a name (MyClass or
+            'MyClass')
+            factory: the callable that can create the above type.
+            singleton: if True, the factory will be invoked exactly once.  If False, the factory
+            will be invoked every time the service is requested.
+        """
         self._add(type_or_name, (singleton, factory))
 
     def add_service_provider(self, provider):
+        """Add a provider, which provides one or more service factories.
+
+        A provider is a class with one or more members decorated with ServiceRegistry@factory.
+        When you add a provider with this method, each of its decorated members will be added to
+        this registry as a factory.
+        """
         for entry in dir(provider):
             try:
                 provider_function = type(provider).__dict__[entry]
@@ -45,6 +82,7 @@ class ServiceRegistry(Container):
         self._values[key] = value
 
     def __getattr__(self, type_or_name):
+        """Enable property-access style access to registered services, i.e., registry.MyClass."""
         key = type_or_name if isinstance(type_or_name, str) else type_or_name.__name__
         if key in self._values:
             value = self._values[key]
@@ -61,6 +99,7 @@ class ServiceRegistry(Container):
         raise RuntimeError(f'No provider for type: {key}')
 
     def __contains__(self, item) -> bool:
+        """Enable the syntax MyClass in registry."""
         try:
             self.get(item)
             return True
@@ -73,33 +112,42 @@ class ServiceRegistry(Container):
 
     @staticmethod
     def factory(type_or_name: Union[str, Type], singleton: bool = False):
+        """Decorator for a function that can be treated as a factory that provides a service.
+
+        Args:
+            type_or_name: the concrete type of the provided service, or a name (MyClass or
+            'MyClass')
+            singleton: if True, the factory will be invoked exactly once.  If False, the factory
+            will be invoked every time the service is requested.
+        """
         def _decorator(original):
             setattr(original, 'factory_provider', (type_or_name, singleton))
             return original
 
         return _decorator
 
-    @staticmethod
-    def service(type_or_name: Union[str, Type]):
-        def _decorator(original):
-            setattr(original, 'value_provider', type_or_name)
-            return original
-        return _decorator
+    #
+    # The below are convenience-wrappers that make it easier to retrieve well-known services.
+    #
 
     @property
     def session_tc(self) -> 'TcSession':
+        """Convenience wrapper to return a TcSession."""
         return self.TcSession
 
     @property
     def redis_client(self) -> 'Redis':
+        """Convenience wrapper to return a Redis client object (redis.Redis)."""
         return self.Redis
 
     @property
     def key_value_store(self) -> Union['KeyValueRedis', 'KeyValueApi']:
+        """Convenience wrapper to return a KeyValue object, either an API version or a Redis one."""
         return self.KeyValueStore
 
     @property
     def playbook(self) -> 'Playbook':
+        """Convenience wrapper to return a Playbook object."""
         return self.Playbook
 
 
